c3e008f347c0eb26f71507157438580d723402bf
Kennedy <kiplangatkennedy019@gmail.com>
2023-03-30 15:03:51 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code here!
+// const ul = document.createElement("ul");
+
+// for (let i = 0; i < 3; i++) {
+//   const li = document.createElement("li");
+//   li.textContent = (i + 1).toString();
+//   ul.append(li);
+// }
+
+// element.append(ul);
+
+const main = document.querySelector("main");
+main.remove();
+const newHeader= document.createElement('h1')
+ newHeader.id="victory"; //or the code below
+ //newHeader.setAttribute("id", "victory"); 
+ newHeader.innerHTML="ooh this amazing";
